f36ee36c97f7e3c0b390cc74084efef7c5c21dd3
Ricardo Franco <ricardosf195@gmail.com>
2022-04-25 22:57:33 +0100

    added comments in Hero

--- src/mummymaze/Hero.java
+++ src/mummymaze/Hero.java
@@ -8,6 +8,8 @@ public class Hero extends Being{
     @Override
     public void move(int number, String direction, MummyMazeState state) {
         super.move(number, direction, state);
+        // se nao verificarmos se o heroi morreu, o heroi nao morre quando cai numa armadilha
+        // (nao sei porque)
         if (!isBeingDead(state)){
             state.getMatrix()[line][column] = this.symbol;
             updateGUI(state);
