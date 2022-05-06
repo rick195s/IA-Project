261393cc864abffabd2f69b4c51d6f7a6bc761e7
Guilherme Lino <lino000000@hotmail.com>
2022-05-06 10:25:58 +0100

    update Being

--- src/mummymaze/Being.java
+++ src/mummymaze/Being.java
@@ -1,15 +1,13 @@
 package mummymaze;
 
 public abstract class Being {
-    protected int line;
-    protected int column;
+    Cell cellBeing;
     protected char symbol;
     private boolean moved;
     private char onTopOf;
 
     public Being(int line, int column, char symbol) {
-        this.line = line;
-        this.column = column;
+        cellBeing = new Cell(line, column);
         this.symbol = symbol;
         moved = false;
         onTopOf = '.';
@@ -19,26 +17,26 @@ public abstract class Being {
         // so pode ir para cima se nao estiver na primeira linha jogavel
         // se na linha acima estiver uma parede '-' o heroi nao pode subir
         // se tiver uma porta em cima nao pode mover para cima
-        return  line > 2 && matrix[line-1][column] != '-' && matrix[line-1][column] != '=';
+        return  cellBeing.getLine() > 2 && matrix[cellBeing.getLine()-1][cellBeing.getColumn()] != '-' && matrix[cellBeing.getLine()-1][cellBeing.getColumn()] != '=';
     }
     public boolean canMoveRight(char[][] matrix) {
         // so pode ir para a direita se nao estiver na última coluna jogavel
         // se tiver uma parede à direita nao pode mover para a direita
         // se tiver uma porta à diretia nao pode mover para a direita
-        return column < matrix.length - 2 && matrix[line][column+1] != '|' && matrix[line][column+1] != '"';
+        return cellBeing.getColumn() < matrix.length - 2 && matrix[cellBeing.getLine()][cellBeing.getColumn()+1] != '|' && matrix[cellBeing.getLine()][cellBeing.getColumn()+1] != '"';
     }
     public boolean canMoveDown(char[][] matrix) {
         // so pode ir para baixo se nao estiver na última linha jogavel
         // se tiver na ultima linha nao pode mover para baixo
         // se tiver uma porta abaxio nao pode mover para baixo
-        return line < matrix.length - 2 && matrix[line+1][column] != '-' && matrix[line+1][column] != '=';
+        return cellBeing.getLine() < matrix.length - 2 && matrix[cellBeing.getLine()+1][cellBeing.getColumn()] != '-' && matrix[cellBeing.getLine()+1][cellBeing.getColumn()] != '=';
     }
 
     public boolean canMoveLeft(char[][] matrix) {
         // so pode ir para a esquerda se nao estiver na primeira coluna jogavel
         // se tiver uma parede à esquerda nao pode mover para a esquerda
         // se tiver uma porta à esquerda nao pode mover para a esquerda
-        return column > 2 && matrix[line][column-1] != '|' && matrix[line][column-1] != '"';
+        return cellBeing.getColumn() > 2 && matrix[cellBeing.getLine()][cellBeing.getColumn()-1] != '|' && matrix[cellBeing.getLine()][cellBeing.getColumn()-1] != '"';
     }
 
 
@@ -47,18 +45,18 @@ public abstract class Being {
 
         // se o "ser" quando se mexeu ficou em cima de algum elemento diferente de '.'
         // o elemento é reposto onde estava
-        matrix[line][column] = onTopOf;
+        matrix[cellBeing.getLine()][cellBeing.getColumn()] = onTopOf;
 
         if (direction.equals("column")){
-            column += number;
+            cellBeing.setColumn(cellBeing.getColumn() + number);
         }else if (direction.equals("line")){
-            line += number;
+            cellBeing.setLine(cellBeing.getLine() + number);
         }
 
         // se um "ser" depois de se mexer ficar em cima de um outro "ser"
         // o "ser" sobreposto não é reposto
-        if (matrix[line][column] != StateRepresentation.HERO && matrix[line][column] != StateRepresentation.WHITEMUMMY && matrix[line][column] != StateRepresentation.REDMUMMY && matrix[line][column] != StateRepresentation.SCORPION) {
-            onTopOf = matrix[line][column];
+        if (matrix[cellBeing.getLine()][cellBeing.getColumn()] != StateRepresentation.HERO && matrix[cellBeing.getLine()][cellBeing.getColumn()] != StateRepresentation.WHITEMUMMY && matrix[cellBeing.getLine()][cellBeing.getColumn()] != StateRepresentation.REDMUMMY && matrix[cellBeing.getLine()][cellBeing.getColumn()] != StateRepresentation.SCORPION) {
+            onTopOf = matrix[cellBeing.getLine()][cellBeing.getColumn()];
         }else {
             onTopOf = '.';
         }
